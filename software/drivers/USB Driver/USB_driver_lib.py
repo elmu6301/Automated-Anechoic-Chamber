b2954b1987839cfc9bf875e4ac0f3ad6e61517de
Elena Murray <elmu6301@colorado.edu>
2020-10-23 11:27:20 -0600

    pyusb now working and is able to detect a device (my old ipos)

--- software/drivers/USB Driver/USB_driver_lib.py
+++ software/drivers/USB Driver/USB_driver_lib.py
@@ -5,8 +5,12 @@ This file contains the USB driver library.
 """
 
 # Include
+import usb.core
+import usb.util
+from usb.backend import libusb1
 
-
+appleId = 0x05AC
+ipodId = 0x1265
 # Developer Includes
 '''
 :param phi
@@ -16,8 +20,13 @@ def set_orientation(phi, theta):
     print(f"Setting orientation to ({phi}, {theta})")
     return
 
-def connect_to_device(vendorId, productId, timeout):
+def connect_to_device(vendorId, productId, timeout=None):
     print(f"Connecting to device with vendorId '{vendorId}' and productId '{productId}")
+    dev = usb.core.find(idVendor=vendorId, idProduct=productId)
+    if dev is None:
+        raise ValueError(f"Could not connect to device with vendorId '{vendorId}' and productId '{productId}")
+    else:
+        print("Connected")
     return
 
 def test_connection(vendorId, productId, timeout):
@@ -26,10 +35,20 @@ def test_connection(vendorId, productId, timeout):
 # main
 def main():
     print("\nRunning USB_driver_lib.py")
-    connect_to_device(0xFFE, 0x1EF, 100)
-    test_connection(0xFFE, 0x1EF, 100)
-    set_orientation(180, 90)
+    connect_to_device(appleId, ipodId)
+    # test_connection(0xFFE, 0x1EF, 100)
+    # set_orientation(180, 90)
 
 
 if __name__ == "__main__":
     main()
+    # import os
+    # os.environ['PYUSB_DEBUG'] = 'debug'
+    # import usb.core
+    # usb.core.find()
+    # be = libusb1.get_backend()
+    # if be is None:
+    #     print("no backend")
+
+    # for device in usb.core.find(find_all=True):
+    #     print(device)
