8aa103edb8d73adbfd838d896baa466770fde165
Elena Murray <elmu6301@colorado.edu>
2020-11-24 10:59:43 -0800

    Added a Janky UI and Updated usb functions to be a class

--- software/main.py
+++ software/main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from drivers import MSP430_usb as usb
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_menu():
+    print("~~~~~~~~~Menu~~~~~~~~~~")
+    print("'o' : set orientation")
+    print("'w' : write data")
+    print("'h' : get help")
+    print("'q' : quit")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Welcome to MSP430 User Interface v0.0")
+    print_menu()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    active = True
+
+    port = input("MSP430 COM port: ")
+    msp430 = usb.MSP430(port, open=False)
+    port_conn = msp430.connect_to_port()
+
+    while not port_conn:
+        print(f"Unable to connect to {port} try a different port or entering port like: COMXX")
+        port = input("MSP430 COM port: ")
+        msp430.port = port
+        port_conn = msp430.connect_to_port()
+
+    while active:
+        action = input("--> ")
+        if action == 'o':
+            phi = input("phi: ")
+            theta = input("theta: ")
+            print(f"Sending {phi,theta} to device...")
+        elif action == 'w':
+            data = input("data: ")
+            res = msp430.write_to_device(data)
+            if res:
+                print("Data transfer was successful!")
+            else:
+                print("Data transfer was not successful!")
+        elif action == 'h':
+            print_menu()
+        elif action == 'q':
+            print(f"Closing port {port}")
+            msp430.disconnect_from_port()
+            active = False
+        else:
+            print("Command not recognized. Enter 'h' for help")
---
+++ software/tests/MSP430_usb_test.py
@@ -0,0 +1,80 @@
+import pytest
+from drivers import MSP430_usb as usb
+import time
+import random
+
+port = 'COM12'
+msp430 = usb.MSP430(port, open=False)
+
+
+def test_to_usb_str():
+    # test with string "LED ON"
+    test_str = "LED ON"
+    usb_str = usb.to_usb_str(test_str)
+    expected = b'LED ON\r'
+    assert usb_str == expected
+    print(f"\n\tCase 'LED ON': expected {expected} got {usb_str}")
+
+    # test with None
+    test_str = None
+    usb_str = usb.to_usb_str(test_str)
+    assert usb_str == None
+    print(f"\tCase 'None': expected {None} got {usb_str}")
+
+
+def test_set_orientation():
+    # TODO
+    assert True
+
+
+def test_connect_to_port():
+    print("\n\t", end='')
+    # test connecting to an unopened port
+    assert msp430.connect_to_port()
+    print("\t", end='')
+    # test connecting to an opened port
+    assert not msp430.connect_to_port()
+
+
+def test_write_to_device():
+    # Turn the LED ON
+    res = msp430.write_to_device('LED ON')
+    assert res == "ACK\n"
+    print(f"\n\tCase CMD = 'LED ON': expected 'ACK' got {res}", end='')
+    time.sleep(1)
+#
+    # Turn the LED OFF
+    res = msp430.write_to_device('LED OFF')
+    assert res == "ACK\n"
+    print(f"\tCase CMD = 'LED OFF': expected 'ACK' got {res}", end='')
+    # time.sleep(1)
+
+    # Send an Unknown Command
+    res = msp430.write_to_device('BAD CMD')
+    assert res == "NACK\n"
+    print(f"\tCase CMD = 'BAD CMD': expected 'NACK' got {res}", end='')
+    # time.sleep(1)
+
+    # Send a command with a random integer
+    rand_int = random.randint(1, 1000)
+    rand_str = "RAND%d" % rand_int
+    res = msp430.write_to_device(rand_str)
+    assert int(res) == rand_int
+    print(f"\tCase CMD = '{rand_str}': expected '{rand_int}' got {res}", end='')
+
+    # Test with None
+    res = msp430.write_to_device(None)
+    assert not res
+    print(f"\tCase CMD = None: expected 'False' got '{res}' ")
+   # time.sleep(1)
+
+
+def test_disconnect_from_port():
+    # try disconnect from an existing port, should pass
+    res = msp430.disconnect_from_port()
+    assert res
+    print(f"\n\tCase existing port: expected 'True' got {res}")
+    # try disconnect from a non-existing port, should fail
+    res = msp430.disconnect_from_port()
+    print(f"\n\tCase non-existing port: expected 'False' got {res}")
+    assert not res
