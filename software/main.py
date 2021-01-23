a0a9d67dc23a67a5a7ab23d0b0f14e8e4cbc66d7
Elena Murray <elmu6301@colorado.edu>
2021-01-22 19:33:35 -0700

    Connected main ui with command line parser

--- software/main.py
+++ software/main.py
@@ -23,61 +23,53 @@ def process_cmd_line(argv):
     except getopt.GetoptError as err:
         print("Error invalid input")
         sys.exit()
-    test = ''
     config = ''
     # Parse all options
     for opt, arg in opts:
         if opt == "-h":
             print_usage()
             sys.exit()
-        elif opt == "-t":
-            print("Arg: " + arg)
-            test = arg
         elif opt == '-c':
             print("Arg: " + arg)
             config = arg
-    print("Test: " + test)
-    print("Config: " + config)
+    print("Opening config: " + config)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     process_cmd_line(sys.argv[1:])
-    # print("Welcome to MSP430 User Interface v0.0")
-    # active = True
-    # while active:
-    #     argv = input("--> ")
-    #     process_cmd_line(argv)
 
-    #
-    # port = input("MSP430 COM port: ")
-    # msp430 = usb.MSP430(port, open=False)
-    # port_conn = msp430.connect_to_port()
-    #
-    # while not port_conn:
-    #     print(f"Unable to connect to {port} try a different port or entering port like: COMXX")
-    #     port = input("MSP430 COM port: ")
-    #     msp430.port = port
-    #     port_conn = msp430.connect_to_port()
-    #
-    # print_menu()
-    # while active:
-    #     action = input("--> ")
-    #     if action == 'o':
-    #         phi = input("phi: ")
-    #         theta = input("theta: ")
-    #         print(f"Sending {phi, theta} to device...")
-    #     elif action == 'w':
-    #         data = input("data: ")
-    #         res = msp430.write_to_device(data)
-    #         if res == 'NACK\n':
-    #             print("Data transfer was not successful!")
-    #         else:
-    #             print("Data transfer was successful!")
-    #     elif action == 'h':
-    #         print_menu()
-    #     elif action == 'q':
-    #         print(f"Closing port {port}")
-    #         msp430.disconnect_from_port()
-    #         active = False
-    #     else:
-    #         print("Command not recognized. Enter 'h' for help")
+    print("Welcome to MSP430 User Interface v0.0")
+    active = True
+    port = input("MSP430 COM port: ")
+    msp430 = usb.MSP430(port, open=False)
+    port_conn = msp430.connect_to_port()
+
+    while not port_conn:
+        print(f"Unable to connect to {port} try a different port or entering port like: COMXX")
+        port = input("MSP430 COM port: ")
+        msp430.port = port
+        port_conn = msp430.connect_to_port()
+
+    print_menu()
+    while active:
+        action = input("--> ")
+        if action == 'o':
+            phi = input("phi: ")
+            theta = input("theta: ")
+            print(f"Sending {phi, theta} to device...")
+        elif action == 'w':
+            data = input("data: ")
+            res = msp430.write_to_device(data)
+            if res == 'NACK\n':
+                print("Data transfer was not successful!")
+            else:
+                print("Data transfer was successful!")
+        elif action == 'h':
+            print_menu()
+        elif action == 'q':
+            print(f"Closing port {port}")
+            msp430.disconnect_from_port()
+            active = False
+        else:
+            print("Command not recognized. Enter 'h' for help")
