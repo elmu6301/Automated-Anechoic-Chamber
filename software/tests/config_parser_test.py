ec9206d10f6005b51cafd5423bd0c6333378fefb
Elena Murray <elmu6301@colorado.edu>
2021-02-18 10:37:31 -0700

    Created a config_parser test file and starting to work on structure

---
+++ software/tests/config_parser_test.py
@@ -0,0 +1,24 @@
+import pytest
+from util import config_parser as cfg
+import time
+import random
+
+
+def test_get_path():
+    cfg.get_path()
+    assert True
+
+
+def test_find_config():
+    cfg.find_config()
+    assert True
+
+
+def test_get_expt_flow():
+    cfg.get_expt_flow()
+    assert True
+
+
+def test_gen_expt_cmds():
+    cfg.gen_expt_cmds()
+    assert True
--- software/util/config_parser.py
+++ software/util/config_parser.py
@@ -8,7 +8,23 @@ This file contains functions to find, open, and.
 '''
 
 
-def find_config(file_name,file_path):
+def get_path():
+    return True
+
+
+def find_config():
+    assert True
+
+
+def get_expt_flow():
+    assert True
+
+
+def gen_expt_cmds():
+    assert True
+
+
+def find_config1(file_name,file_path):
     # Check to see if file_name is valid
     if file_name is None or file_name == '':
         print("Error: No config file detected...")
@@ -18,12 +34,26 @@ def find_config(file_name,file_path):
     cfg = open(file_name, "r")
 
 
-
-
-
 # main
 def main():
+
     print("\nRunning the config parser")
+
+    # Testing reading/loading ability
+    data = None
+    flow = None
+    with open("../configs/sample_exp.json", "r") as file:
+        data = json.load(file)
+        flow = data.get("flow")
+        print(flow)
+    if data is None:
+        print("Could not find/load file...")
+    # else:
+    #     print(data)
+
+    # Testing data parsing
+    for expt in flow:
+        print("Found experiment: "+expt.get("expType"))
 
 
 if __name__ == "__main__":
