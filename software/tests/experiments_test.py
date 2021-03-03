5313fc26fc29b1a7f4251d689784eade537250a6
Elena Murray <elmu6301@colorado.edu>
2021-03-03 12:13:37 -0700

    Generating a pytest for experiments.py

---
+++ software/tests/experiments_test.py
@@ -0,0 +1,15 @@
+import pytest
+from util import experiments as expt
+import time
+import random
+
+
+def test_gen_sweepPhi_cmds():
+    assert True
+
+
+def test_gen_sweepTheta_cmds():
+    assert True
+
+def test_gen_sweepFreq_cmds():
+    assert True
